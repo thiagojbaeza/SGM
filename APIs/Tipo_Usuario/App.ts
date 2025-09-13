59bbd99dcafb89ee22487d3cbcaba1d4d256c7bc
Thiago J Baeza <thiagojbaeza@gmail.com>
2025-09-13 15:50:58 -0300

    Inclusão da API de Login e Tipo Usuario

---
+++ APIs/Tipo_Usuario/App.ts
@@ -0,0 +1,74 @@
+import fastify from 'fastify';
+import { LoginController } from './Controllers/LoginController.ts';
+import type { ILogin } from './Database/Entities/Login.ts';
+
+
+const api = fastify({
+    logger: true
+});
+
+const controller = new LoginController();
+
+api.post('/login', async function (request, response) {
+    const usuario = request.headers["usuario"] as string;
+    const senha = request.headers["senha"] as string;
+    const data = await controller.login(usuario, senha);
+    response.send({ result: data});
+});
+
+api.post('/createLogin', async function (request, response) {
+    const body = request.body as ILogin;
+    const token = request.headers["token"] as string;
+    const data = await controller.createLogin(body, token); 
+    if(data.success === true){
+        response.send({ result:"Registro inserido com sucesso!"});
+    }else{
+        response.send({ result:"Houve um problema ao inserir o registro!"});
+    }
+});
+
+
+/* api.get('/:id', async function (request, response) {
+    const { id } = request.params as { id: string };
+    var data = await controller.getUserById(id);    
+    response.send({ result: data.length ? data: "Id nao encontrado! verifique infos."});
+});
+
+api.post('/', async function (request, response) {
+    var body = request.body as IUser;
+    var data = await controller.addUser(body);    
+    response.send({ result:"Registro inserido com sucesso!"});
+});
+
+api.put('/', async function (request, response) {
+    var body = request.body as IUser;
+    var data = await controller.updateUser(body);    
+    response.send({ result:"Registro alterado com sucesso!"});
+});
+
+api.delete('/:id', async function (request, response) {
+    const { id } = request.params as { id: string };
+    var data = await controller.deleteUser(id);    
+    response.send({ result:"Registro deletado com sucesso!"});
+});
+ */
+
+
+
+
+/* api.get('/', async function (request, response) {
+    const token = request.headers["token"] as string;
+    const data = await controller.getUser(token);    
+    response.send({ result: data});
+}); */
+
+const start = async () => {
+    try {
+        await api.listen({port: 3000})
+    }catch(err){
+        api.log.error(err)
+        process.exit(1)
+    }
+}
+
+start()
---
+++ APIs/Tipo_Usuario/Controllers/LoginController.ts
@@ -0,0 +1,61 @@
+import { LoginDAO, type ILogin } from "../Database/Entities/Login.ts";
+import { Token } from "../Utils/token.ts";
+
+
+
+export class LoginController{
+    private loginEntity = new LoginDAO();
+    private token = new Token(); 
+
+    public async login(login:string, senha:string){
+        const result = await this.loginEntity.getLogin(login, senha);
+        if(result === undefined){
+            return {success: false, token: null}
+        }else{
+            return {success: true, token: this.token.createToken(login)}
+        }
+    }
+
+    public async createLogin(login: ILogin, token:string){
+        const validateToken = this.token.validateToken(token);
+        if(validateToken == false){
+            return {success: false, token: null}
+        }else{
+            await this.loginEntity.addLogin(login);
+            return {success: true}
+        }
+    }
+
+
+/*     public async getUser(token: string){
+        if(this.token.validateToken(token)){
+            const result = await this.entity.getUser();
+            return {payload: result, success: true};
+        }else{
+            return {payload: null, success: false};
+        }
+    }
+
+    public async getUserById(id_user:string){
+        const result = await this.entity.getUserById(id_user);
+        return result;
+    }
+
+    public async addUser(user:IUser){
+        const result = await this.entity.addUser(user);
+        return result;
+    }
+
+    public async updateUser(user:IUser){
+        const result = await this.entity.updateUser(user);
+        return result;
+    }
+
+    public async deleteUser(id_user: string){
+        const result = await this.entity.deleteUser(id_user);
+        return result;
+    } */
+}
+
+
+
