a02add084dc16fb41b5834e648269b994aab7968
Thiago J Baeza <thiagojbaeza@gmail.com>
2025-09-14 19:08:04 -0300

    inclusao do BD

---
+++ APIs/Maquina/App.ts
@@ -0,0 +1,56 @@
+import fastify from 'fastify';
+import { TipoUsuarioController } from './Controllers/MaquinaController.ts';
+import type { ITipoUsuario } from './Database/Entities/Maquina.ts';
+
+const api = fastify({
+    logger: true
+});
+
+const controller = new TipoUsuarioController();
+
+api.get('/:id', async function (request, response) {
+    const { id } = request.params as { id: string };
+    const token = request.headers["token"] as string;
+    const data = await controller.getTipoUsuario(token, id);    
+    response.send({ result: data.payload?.length ? data.payload : "Id nao encontrado! verifique infos.", 
+        success: data.payload!.length >0 });
+});
+
+api.post('/', async function (request, response) {
+    const token = request.headers["token"] as string;
+    const data = await controller.addTipoUsuario(token, request.body as ITipoUsuario);    
+    if(data.success === true){
+        response.send({ result:"Registro inserido com sucesso!"});
+    }else{
+        response.send({ result:"Houve um problema ao inserir o registro!"});
+    }
+});
+
+api.put('/:id', async function (request, response) {
+    const token = request.headers["token"] as string;
+    const { id } = request.params as { id: string };
+    const data = await controller.updateTipoUsuario(token, request.body as ITipoUsuario, id);    
+    if(data.success === true){
+        response.send({ result:"Registro alterado com sucesso!"});
+    }else{
+        response.send({ result:"Houve um problema ao alterar o registro!"});
+    }
+});
+
+api.delete('/:id', async function (request, response) {
+    const token = request.headers["token"] as string;
+    const { id } = request.params as { id: string };
+    await controller.deleteTipoUsuario(token,request.body as ITipoUsuario, id);    
+    response.send({ result:"Registro deletado com sucesso!"});
+});
+
+const start = async () => {
+    try {
+        await api.listen({port: 3000})
+    }catch(err){
+        api.log.error(err)
+        process.exit(1)
+    }
+}
+
+start()
---
+++ APIs/Maquina/Database/Entities/Maquina.ts
@@ -0,0 +1,50 @@
+import { Db } from "../Db.ts";
+
+export interface IMaquina{
+    id_tipo_usuario?: number;
+    ds_tipo_usuario: string;
+    fg_ativo?: number;
+    dt_criacao?: string;
+    id_usuario_criacao: number;
+    dt_ultima_alteracao?: string;
+    id_usuario_ultima_alteracao?: number;
+}
+
+export class MaquinaDAO{
+    private db: Db;
+
+    constructor(){
+        this.db = new Db();
+    }
+
+     public async getMaquina(id_tipo_usuario?: string){
+        let sql = "SELECT id_tipo_usuario, ds_tipo_usuario FROM tb_tipo_usuario WHERE fg_ativo = 1";
+        
+        if(id_tipo_usuario){
+            sql += " AND id_tipo_usuario = ?";
+        }
+        
+        const result = await this.db.query(sql, [id_tipo_usuario]);
+        return result as IMaquina[];
+    } 
+
+    public async addMaquina(tipoUsuario: IMaquina){
+        const sql = "insert into tb_tipo_usuario (ds_tipo_usuario, id_usuario_criacao) values(?,?)";
+        await this.db.execute(sql, [tipoUsuario.ds_tipo_usuario, tipoUsuario.id_usuario_criacao]);
+    } 
+
+    public async updateMaquina(maquina: IMaquina, id:string){
+        const sql = "update tb_tipo_usuario set ds_tipo_usuario=?, fg_ativo =?, dt_ultima_alteracao =NOW(), id_usuario_ultima_alteracao=? where id_tipo_usuario =? ";
+        await this.db.execute(sql, [tipoUsuario.ds_tipo_usuario, tipoUsuario.fg_ativo, tipoUsuario.id_usuario_ultima_alteracao, id]);
+    } 
+
+/*     public async updateTipoUsuario(tipoUsuario: ITipoUsuario, id:string){
+        const sql = "update tb_tipo_usuario set ds_tipo_usuario=?, fg_ativo=?, dt_ultima_alteracao=NOW(), id_usuario_ultima_alteracao=? where id_tipo_usuario =? ";
+        await this.db.execute(sql, [tipoUsuario.ds_tipo_usuario,tipoUsuario.fg_ativo, tipoUsuario.id_usuario_ultima_alteracao, id]);
+    }  */
+        
+    public async deleteMaquina(maquina: IMaquina, id:string){
+        const sql = "update tb_tipo_usuario set fg_ativo =0, dt_ultima_alteracao =NOW(), id_usuario_ultima_alteracao=? where id_tipo_usuario =? ";
+        await this.db.execute(sql, [tipoUsuario.id_usuario_ultima_alteracao, id]);
+    } 
+}
