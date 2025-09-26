dc9b48e26a75330a8d7f5c7f82d6c1c9e3ec3638
Thiago J Baeza <thiagojbaeza@gmail.com>
2025-09-26 14:26:06 -0300

    Inclusao do nome do usuario logado na tela

--- Web/AdminPanel/src/components/header/page.jsx
+++ Web/AdminPanel/src/components/header/page.jsx
@@ -1,21 +1,37 @@
 import './Header.css';
 import { FiMenu } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
 
 export default function Header({ darkMode, setDarkMode, toggleSidebar }) {
+  const [nameUser, setNameUser] = useState('');
+
+  useEffect(() => {
+    const storedName = localStorage.getItem('nameUser');
+    if (storedName) {
+      setNameUser(storedName);
+    }
+  }, []);
+
   return (
     <header className="header">
       <button className="menu-button" onClick={toggleSidebar}>
         <FiMenu size={24} />
       </button>
+
       <h1>Movelaria Ponce</h1>
-      <label className="switch">
-        <input
-          type="checkbox"
-          checked={darkMode}
-          onChange={() => setDarkMode(!darkMode)}
-        />
-        <span className="slider"></span>
-      </label>
+
+      <div className="header-right">
+        <span className="welcome-text">Bem-vindo(a): {nameUser}</span>
+
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={darkMode}
+            onChange={() => setDarkMode(!darkMode)}
+          />
+          <span className="slider"></span>
+        </label>
+      </div>
     </header>
   );
 }
